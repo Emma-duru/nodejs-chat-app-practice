1be5b5617bc7b97af87c80a08d4f129dc3359442
Emma-duru <60073539+Emma-duru@users.noreply.github.com>
2020-11-14 23:08:32 +0100

    Remove change name event listener

--- public/chat.js
+++ public/chat.js
@@ -3,9 +3,6 @@ const socket = io();
 const chatBtn = document.querySelector("#chat-send");
 const chatInput = document.querySelector("#chat-input");
 const chatList = document.querySelector(".chat-list");
-const nameBtn = document.querySelector("#name-send");
-const nameInput = document.querySelector("#name-input");
-const username = document.querySelector(".username");
 
 chatBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -14,20 +11,10 @@ chatBtn.addEventListener("click", (e) => {
   chatInput.value = "";
 });
 
-nameBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const name = nameInput.value;
-  socket.emit("change_name", name);
-  nameInput.value = "";
-});
 
 socket.on("send_chat", (data) => {
   chatList.innerHTML += `
         <p class="chat-item" style="background-color:${data.color}">${data.msg}</p>
     `;
   chatList.scrollTop = chatList.scrollHeight;
-});
-
-socket.on("change_name", (name) => {
-  username.innerHTML = name;
 });
